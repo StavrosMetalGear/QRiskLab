1f09fb2b0bc70b2592d8a89562732f3db2150729
Stavros Zymvragoudakis <stavrosiu@hotmail.com>
2026-05-14 21:04:03 +0300

    Add project files.

---
+++ QRiskLab/QRiskLab.cpp
@@ -0,0 +1,12 @@
+﻿// QRiskLab.cpp : Defines the entry point for the application.
+//
+
+#include "QRiskLab.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
